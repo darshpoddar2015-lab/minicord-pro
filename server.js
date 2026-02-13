51d87bd2ea1e658ebf4b4d0e0df2073ec22e3fe4
nalini tiwari <darshpoddar2015@gmail.com>
2026-02-13 18:15:51 +0530

    fix

--- server.js
+++ server.js
@@ -178,6 +178,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(4000, () => {
-    console.log("Server running on port 4000");
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
